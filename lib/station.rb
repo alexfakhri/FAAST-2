9377770b4fa2d2c77868d17831bc1ae11363fba3
alexfakhri <alexfakhri@me.com>
2014-10-01 18:08:41 +0100

    Station created with touch in method

---
+++ lib/station.rb
@@ -0,0 +1,15 @@
+class Station
+
+def initialize 
+	@passengers = []
+end
+
+def passenger_count
+	@passengers.count
+end
+
+def touch_in(passenger)
+	@passengers << passenger
+end
+
+end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,8 +1,13 @@
 require 'station'
 
 describe Station do
+
+	let(:station) { Station.new }
+	let(:passenger) { double :passenger}
 	
-	
+	it 'Should allow passengers to touch into the station' do
+		expect{station.touch_in(passenger)}.to change{station.passenger_count}.by 1
+	end
 
 
 
